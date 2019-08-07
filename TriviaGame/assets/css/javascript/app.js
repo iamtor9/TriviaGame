d96f444786e3424c253c74ef056574162de4fdab
iamtor9 <iamtor9@gmail.com>
2019-08-07 17:44:58 -0500

    linking stylesheets

---
+++ TriviaGame/assets/css/javascript/app.js
@@ -0,0 +1,23 @@
+//create a trivia form with true/false options (your choice).
+
+let questions = [Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9]
+
+let playerGuess =[" "]
+let a= "true"
+let b= "false"
+
+function Q0 (_) {
+    
+}
+//create countdown timer
+document.getElementById.clockTimer
+
+//The player will have a limited amount of time to finish the quiz. 
+// make a timer to begin when the player hits the start button
+//have timer start and end, after player has chosen their answers
+// or if player runs out of time, they lose, game returns to start
+//The game ends when the time runs out. 
+//The page will reveal the number of questions 
+//that player answers correctly and incorrectly.
+//player picks only one answer per question.
+
